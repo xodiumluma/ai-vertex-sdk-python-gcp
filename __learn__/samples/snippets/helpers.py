0eda6029f15d0968b96a26a90294319788e46a41
xodiumluma <28709874+xodiumluma@users.noreply.github.com>
2024-06-22 17:57:06 +1000

    [learn] featurestore resource name

--- __learn__/samples/snippets/helpers.py
+++ __learn__/samples/snippets/helpers.py
@@ -6,7 +6,11 @@ from timeit import default_timer as timer
 from typing import Callable
 
 def get_name(out, key="name"):
-  pattern = re.compile(rf'{key}:\s*"([\-a-zA-Z0-9/]+)"')
+  pattern = re.compile(r"state:\s*([._a-zA-Z0-9/]+)")
   name = re.search(pattern, out).group(1)
+  return name
 
-  return
+def get_featurestore_resource_name(out, key="name"):
+  pattern = re.compile(rf'{key}:\s*"([\_\-a-zA-Z0-9]+)"')
+  name = re.search(pattern, out).group(1)
+  return name
