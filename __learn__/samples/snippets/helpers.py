bc1260d92cc15833eca959a814c5b6339501f5cc
xodiumluma <28709874+xodiumluma@users.noreply.github.com>
2024-06-22 17:50:20 +1000

    [learn] get_name function

--- __learn__/samples/snippets/helpers.py
+++ __learn__/samples/snippets/helpers.py
@@ -1,3 +1,12 @@
 import collections
 import re
 import time
+from timeit import default_timer as timer
+
+from typing import Callable
+
+def get_name(out, key="name"):
+  pattern = re.compile(rf'{key}:\s*"([\-a-zA-Z0-9/]+)"')
+  name = re.search(pattern, out).group(1)
+
+  return
