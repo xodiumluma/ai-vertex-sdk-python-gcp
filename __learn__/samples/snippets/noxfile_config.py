c33400f21d7d8b6727f0c99983d79112924697c8
xodiumluma <28709874+xodiumluma@users.noreply.github.com>
2024-07-07 21:48:58 +1000

    [learn] finish noxfile config

--- __learn__/samples/snippets/noxfile_config.py
+++ __learn__/samples/snippets/noxfile_config.py
@@ -6,4 +6,11 @@ TEST_CONFIG_OVERRIDE = {
   "ignored_versions": ["2.7", "3.6", "3.8", "3.9"],
   # populate at will
   "gcloud_project_env": "BUILD_SPECIFIC_GOOGLE_CLOUD_PROJECT",
+  # a dictionary relevant to your test which will be injected into your test
+  # Please don't include secrets
+  # All previous values will be overridden
+  "envs": {
+    "DATA_LABELLING_API_ENDPOINT": "us-central1-autopush-aiplatform.sandbox.googleapis.com",
+    "PYTEST_ADDOPTS": "-n=auto" # with all available CPUs run tests in parallel
+  },
 }
