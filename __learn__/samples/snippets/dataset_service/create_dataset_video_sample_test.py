dd5ebf979e15468ae1391b92a22fb2e9965fcde0
xodiumluma <28709874+xodiumluma@users.noreply.github.com>
2024-08-28 12:43:12 +1000

    [learn] setup teardown function for video

--- __learn__/samples/snippets/dataset_service/create_dataset_video_sample_test.py
+++ __learn__/samples/snippets/dataset_service/create_dataset_video_sample_test.py
@@ -1,2 +1,16 @@
 import os
 from uuid import uuid4
+
+import create_dataset_video_sample
+import pytest
+
+import helpers
+
+PROJECT_ID = os.getenv("BUILD_SPECIFIC_GCLOUD_PROJECT")
+VIDEO_METADATA_SCHEMA_URI = (
+  "gs://google-cloud-aiplatform/schema/dataset/metadata/video_1.0.0.yaml"
+)
+
+@pytest.fixture(scope="function", autouse=True)
+def teardown(tearown_dataset):
+  yield
