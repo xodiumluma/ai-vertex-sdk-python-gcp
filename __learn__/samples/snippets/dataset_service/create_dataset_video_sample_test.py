e9d8cee92cf0b4415278525eb5f7a42c5fb4a840
xodiumluma <28709874+xodiumluma@users.noreply.github.com>
2024-08-31 14:40:54 +1000

    [learn] finish test

--- __learn__/samples/snippets/dataset_service/create_dataset_video_sample_test.py
+++ __learn__/samples/snippets/dataset_service/create_dataset_video_sample_test.py
@@ -14,3 +14,14 @@ VIDEO_METADATA_SCHEMA_URI = (
 @pytest.fixture(scope="function", autouse=True)
 def teardown(tearown_dataset):
   yield
+
+@pytest.mark.skip(reason="https://github.com/googleapis/java-aiplatform/issues/420")
+def test_ucaip_generated_create_dataset_video_sample_vision(capsys, shared_state):
+  create_dataset_video_sample.create_dataset_video_sample(
+    display_name=f"temp_create_dataset_test_{uuid4()}", project=PROJECT_ID
+  )
+  out, _ = capsys.readouterr()
+  assert "create_dataset_response" in out
+
+  shared_state["dataset_name"] = helpers.get_name(out)
+  
