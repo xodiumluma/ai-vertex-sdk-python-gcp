cba68b30780e8774b48f3514135814cb30a0b0e2
xodiumluma <28709874+xodiumluma@users.noreply.github.com>
2024-09-15 03:55:39 +1000

    [learn] import image classification single label data

---
+++ __learn__/samples/snippets/dataset_service/import_data_image_classification_single_label_sample.py
@@ -0,0 +1,29 @@
+# [START_aiplatform_import_data_image_classification_single_label_sample]
+
+from google.cloud import aiplatform
+
+def import_data_image_classification_single_label_sample(
+  project: str,
+  dataset_id: str,
+  gcs_source_uri: str,
+  location: str = "us-central1",
+  api_endpoint: str = "us-central1-aiplatform.googleapis.com",
+  timeout: int = 1800,
+):
+  # regional endpoints needed
+  client_options = {"api_endpoint": api_endpoint}
+  # instantiate reusable client
+  client = aiplatform.gapic.DatasetServiceClient(client_options=client_options)
+  import_configs = [
+    {
+      "gcs_source": {"uris":[gcs_source_uri]},
+      "import_schema_uri": "gs://google-cloud-aiplatform/schema/dataset/ioformat/image_classification_single_label_io_format_1.0.0.yaml",
+    }
+  ]
+  name = client.dataset_path(project=project, location=location, dataset=dataset_id)
+  response = client.import_data(name=name, import_configs=import_configs)
+  print("lengthy op:", response.operation.name)
+  import_data_response = response.result(timeout=timeout)
+  print("import_data_response:", import_data_response)
+
+# [END aiplatform_import_data_image_classification_single_label_sample]
