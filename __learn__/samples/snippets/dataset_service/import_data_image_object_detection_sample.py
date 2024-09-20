873377413477dc28afd73644d0a58df589047837
xodiumluma <28709874+xodiumluma@users.noreply.github.com>
2024-09-21 08:27:34 +1000

    [learn] complete object detection import

--- __learn__/samples/snippets/dataset_service/import_data_image_object_detection_sample.py
+++ __learn__/samples/snippets/dataset_service/import_data_image_object_detection_sample.py
@@ -13,4 +13,15 @@ def import_data_image_object_detection_sample(
   client_options = {"api_endpoint": api_endpoint}
   # create reusable client
   client = aiplatform.gapic.DatasetServiceClient(client_options=client_options)
+  import_configs = [
+    {
+      "gcs_source": {"uris": [gcs_source_uri]},
+      "import_schema_uri": "gs://google-cloud-platform/schema/dataset/ioformat/image_bounding_box_io_format_1.0.0.yaml",
+    }
+  ]
+  name = client.dataset_path(project=project, location=location, dataset=dataset_id)
+  response = client.import_data(name=name, import_configs=import_configs)
+  print("Lengthy running operation: ", response.operation.name)
+  import_data_response = response.result(timeout=timeout)
+  print("import data response:", import_data_response)
 # [END aiplatform_import_data_image_object_detection_sample]
