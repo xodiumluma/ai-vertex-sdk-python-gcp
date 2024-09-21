2b6fc068d9bcd1e87c2825661fcd3eedd2e183d8
xodiumluma <28709874+xodiumluma@users.noreply.github.com>
2024-09-22 08:25:16 +1000

    [learn] create function to extract text entity

---
+++ __learn__/samples/snippets/dataset_service/import_data_text_entity_extraction_sample.py
@@ -0,0 +1,30 @@
+# [START aiplatform_import_data_text_entity_extraction_sample]
+
+from google.cloud import aiplatform
+
+def import_data_text_entity_extraction_sample(
+  project: str,
+  dataset_id: str,
+  gcs_source_uri: str,
+  location: str = "us-central1",
+  api_endpoint: str = "us-central1-aiplatform.googleapis.com",
+  timeout: int = 1800
+):
+  # regional API endpoints
+  client_options = {"api_endpoint": api_endpoint}
+  # reusable client
+  client = aiplatform.gapic.DatasetServiceClient(client_options=client_options)
+  import_configs = [
+    {
+      "gcs_source": {"uris": [gcs_source_uri]},
+      "import_schema_uri": "gs://google-cloud-aiplatform/schema/dataset/ioformat/text_extraction_io_format_1.0.0.yaml",
+
+    }
+  ]
+  name = client.dataset_path(project=project, location=location, dataset=dataset_id)
+  response = client.import_data(name=name, import_configs=import_configs)
+  print("Lengthy process:", response.operation.name)
+  import_data_response = response.result(timeout=timeout)
+  print("import_data_response:", import_data_response)
+
+# [END aiplatform_import_data_text_entity_extraction_sample]
