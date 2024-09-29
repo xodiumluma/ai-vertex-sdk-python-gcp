e197e5ec1b454be392eb790389dad2e595005f24
xodiumluma <28709874+xodiumluma@users.noreply.github.com>
2024-09-29 12:06:25 +1000

    [learn] finish off textual sentiment analysis

--- __learn__/samples/snippets/dataset_service/import_data_text_sentiment_analysis_sample.py
+++ __learn__/samples/snippets/dataset_service/import_data_text_sentiment_analysis_sample.py
@@ -13,3 +13,16 @@ def import_data_text_sentiment_analysis_sample(
   client_options = {"api_endpoint": api_endpoint}
   # reusable client
   client = aiplatform.gapic.DatasetServiceClient(client_options=client_options)
+  import_configs = [
+    {
+      "gcs_source": {"uris": [gcs_source_uri]},
+      "import_schema_uri": "gs://google-cloud-aiplatform/schema/dataset/ioformat/text_sentiment_io_format_1.0.0.yaml",
+    }
+  ]
+  name = client.dataset_path(project=project, location=location, dataset=dataset_id)
+  response = client.import_data(name=name, import_configs=import_configs)
+  print("Lengthy operation:", response.operation.name)
+  import_data_response = response.result(timeout=timeout)
+  print("import_data_response:", import_data_response)
+
+# [END aiplatform_import_data_text_sentiment_analysis_sample]
