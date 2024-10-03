c6800e8725d5ec7f64466378ad0a3049948d9875
xodiumluma <28709874+xodiumluma@users.noreply.github.com>
2024-10-03 10:18:36 +1000

    [learn] bugfix

--- __learn__/samples/snippets/dataset_service/import_data_video_action_recognition_sample.py
+++ __learn__/samples/snippets/dataset_service/import_data_video_action_recognition_sample.py
@@ -18,10 +18,10 @@ def import_data_video_action_recognition_sample(
       "gcs_source": {"uris": [gcs_source_uri]},
       "import_schema_uri": "gs://google-cloud-aiplatform/schema/dataset/ioformat/video_action_recognition_io_format_1.0.0.yaml",
     }
-    name = client.dataset_path(project=project, location=location, dataset=dataset_id)
-    response = client.import_data(name=name, import_configs=import_configs)
-    print("Length ops:", response.operation.name)
-    import_data_response = response.result(timeout=timeout)
-    print("import_data_response:", import_data_response)
   ]
+  name = client.dataset_path(project=project, location=location, dataset=dataset_id)
+  response = client.import_data(name=name, import_configs=import_configs)
+  print("Length ops:", response.operation.name)
+  import_data_response = response.result(timeout=timeout)
+  print("import_data_response:", import_data_response)
 # [END aiplatform_import_data_video_action_recognition_sample]
