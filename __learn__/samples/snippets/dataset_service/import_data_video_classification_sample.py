f07d0145d64b9f27d0d0f42bc52ae34f578034b8
xodiumluma <28709874+xodiumluma@users.noreply.github.com>
2024-10-11 10:46:28 +1100

    [learn] method params

--- __learn__/samples/snippets/dataset_service/import_data_video_classification_sample.py
+++ __learn__/samples/snippets/dataset_service/import_data_video_classification_sample.py
@@ -1,5 +1,12 @@
 # [START aiplatform_import_data_video_classification_sample]
 from google.cloud import aiplatform
 
-
+def import_data_video_classification_sample(
+  project: str,
+  dataset_id: str,
+  gcs_source_uri: str,
+  location: str = "us-central1",
+  api_endpoint: str = "us-central1-aiplatform.googleapis.com",
+  timeout: int = 1800,
+):
 # [END aiplaform_import_data_video_classification_sample]
