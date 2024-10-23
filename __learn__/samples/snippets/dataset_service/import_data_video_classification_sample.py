648cd5d270cdf69a9a5ee864a3c2fb8a45c30f4b
xodiumluma <28709874+xodiumluma@users.noreply.github.com>
2024-10-24 01:44:09 +1100

    [learn] video classification example

--- __learn__/samples/snippets/dataset_service/import_data_video_classification_sample.py
+++ __learn__/samples/snippets/dataset_service/import_data_video_classification_sample.py
@@ -9,4 +9,19 @@ def import_data_video_classification_sample(
   api_endpoint: str = "us-central1-aiplatform.googleapis.com",
   timeout: int = 1800,
 ):
+  # regional endpoints
+  client_options = {"api_endpoint": api_endpoint}
+  # reusable client
+  client = aiplatform.gapic.DatasetServiceClient(client_options=client_options)
+  import_configs = [
+    {
+      "gcs_source": {"uris": [gcs_source_uri]},
+      "import_schema_uri": "gs://google-cloud-aiplatform/schema/dataset/ioformat/video_classification_io_format_1.0.0.yaml",
+    }
+  ]
+  name = client.dataset_path(project=project, location=location, dataset=dataset_id)
+  response = client.import_data(name=name, import_configs=import_configs)
+  print("Lengthy op: ", response.operation.name)
+  import_data_response = response.result(timeout=timeout)
+  print("import_data_response:", import_data_response)
 # [END aiplaform_import_data_video_classification_sample]
